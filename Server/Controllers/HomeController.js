e3db9fb962fccca0aedc35e7d668f9ccb41ee3b9
Vinith1397 <107805799+Vinith1397@users.noreply.github.com>
2025-08-14 14:20:41 -0700

    Delete Server directory

--- Server/Controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-const HomeController = async(req, res) => {
-    
-    res.send(`Welcome to the MyShopping Hoem PAge this is the Home api 
-        http://localhost:3600/v1/api 
-        `)
-}
-
-
-module.exports = HomeController;
--- Server/Routes/HomeRoute.js
+++
@@ -1,7 +0,0 @@
-console.log('🔌 HomeRoute mounted');
-const express = require('express')
-const router = express.Router()
-const HomeController = require('../Controllers/HomeController')
-router.get('/', HomeController)
-
-module.exports = router
